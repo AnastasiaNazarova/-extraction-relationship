e896f473e688ea6ee8bfca3d4acbec3754c59840
mercurysplash <45242521+mercurysplash@users.noreply.github.com>
2020-06-30 23:48:50 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import nltk
+
+def findrelations(text):
+    sentences = nltk.sent_tokenize(text)# разбить текст на предложения
+    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences] # разбить предложения на токены
+    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]# морфологический разбор
+    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences)#выделение именованных сущностей
+
+    for doc in chunked_sentences:
+        doc.draw()
+
+text= open("test1.txt", 'r', encoding='utf-8').read() #открыть файл русскоязычный файл
+#text= open("test2.txt").read() #открыть файл англоязычный файл
+findrelations(text)
